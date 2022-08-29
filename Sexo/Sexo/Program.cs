7f4636695d35b197931817c1dedc3f0635b534af
Pedro Murback <ninjamurback@gmail.com>
2022-08-29 00:38:09 -0300

    xd
    se liga

---
+++ Sexo/Sexo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Sexo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("se mata tanjas");
+            Console.WriteLine("Você é viado");
+                
+            
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
